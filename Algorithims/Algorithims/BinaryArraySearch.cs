f60b736a119b0d25c48f466f4ed026ae3c6f32e1
cayala <carlosandresayalag@yahoo.com>
2019-01-20 21:49:16 -0800

    This is an initial commit of a collection of arrays implememted into a simple console app

---
+++ Algorithims/Algorithims/BinaryArraySearch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithims.Algorithims
+{
+    public class BinaryArraySearch
+    {
+        public static void Start()
+        {
+            Console.WriteLine("Beginning Binary Search");
+            Console.WriteLine("Number to search for");
+            string searchNum = Console.ReadLine();
+
+            Console.WriteLine("Start Point");
+
+            string startPoint = Console.ReadLine();
+
+            int[] sortedArray = new int[100];
+            for (int x = 0; x < sortedArray.Length; x++)
+            {
+                sortedArray[x] = x;
+            }
+            BinarySearch(sortedArray, int.Parse(searchNum), int.Parse(startPoint), sortedArray.Length);
+        }
+        
+
+        public static void BinarySearch(int[] array, int searchNum, int beginRange, int endRange)
+        {
+
+            int midPoint = ((endRange - beginRange) / 2) + beginRange;
+
+            if (array[midPoint] == searchNum)
+            {
+                Console.WriteLine($"Ding number found: {searchNum} at index: {midPoint}");
+            }
+            else if (searchNum > array[midPoint])
+            {
+                BinarySearch(array, searchNum, midPoint, endRange);
+            }
+            else if (searchNum < array[midPoint])
+            {
+                BinarySearch(array, searchNum, beginRange, midPoint);
+            }
+        }
+    }
+}
